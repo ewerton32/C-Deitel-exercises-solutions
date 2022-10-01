05113cd3746047aa09a7efab189d276b019d246e
ewerton souto <otavio.souto@gmail.com>
2022-10-01 03:32:00 -0300

    # 06 - Exercise 2.1

---
+++ exercises/exe2.1.c
@@ -0,0 +1,14 @@
+//Figura 2.1: fig02_01.cpp
+//Programa de impressão de texto
+
+#include <iostream>//permite que o pregrama gere saida de dados na tela
+
+using namespace std;
+//a função main inici a execução do programa
+int main()
+{
+    cout << "Wecome to C++!\n" << endl;//exibe a mensagem
+
+
+    return 0;//indica que o programa terminou execução com sucesso
+}
