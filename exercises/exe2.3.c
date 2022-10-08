02987aa64f0d326edc96baff691a8db1332c20cf
ewerton souto <otavio.souto@gmail.com>
2022-10-07 21:40:26 -0300

    #09 Exercise 2.4 and 2.3

---
+++ exercises/exe2.3.c
@@ -0,0 +1,16 @@
+//Figura 2.3 fig02_03.cpp
+//Programa de impressão de texto
+
+#include <iostream>//permite que o programa gere saida de dados na tela
+
+using namespaces std;
+
+//a função main inicia a execução do programa
+
+int main(){
+
+cout << "Welcome";
+cout <<" to C++!\n";
+
+return 0; // indica que o programa terminou
+}//fim da função main
---
+++ exercises/exe2.4.c
@@ -0,0 +1,14 @@
+//Figura 2.4: fig02_04.cpp
+//Imprimindo multiplas linhas com uma unica instrução
+
+#include <iostream>
+
+using namespace std;
+
+//função main inicia a execução do programa
+ int main(){
+
+ std::cout << "Welcome \n\n to c++!\n";
+
+ return 0; //finaliza a execução
+ }//fim da função main
